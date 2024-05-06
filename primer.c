6b38b3515529dd853d0ef7369dd9491587b73de7
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-05-05 19:07:33 -0600

    Create primer.c

---
+++ primer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("este es un texto /n");
+    printf("que fue impreso en c");
+    printf("/n");
+    printf("ya soy todo un programador");
+    printf("este es un texto /n");
+    printf("/n");
+    return 0;
+}
